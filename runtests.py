cab6e9810e27e0712f0e127ef15f017b80c6cc13
James Ritchie <james.a.ritchie@gmail.com>
2015-02-21 17:48:16 +0000

    Fix runtest.py to work with Django 1.6

--- runtests.py
+++ runtests.py
@@ -2,13 +2,20 @@
 import os
 import sys
 
-import django
-from django.conf import settings
-from django.test.utils import get_runner
-
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
-    django.setup()
+    from django.conf import settings
+
+    # Will onlu work in 1.7
+    try:
+        from django import setup
+    except ImportError:
+        pass
+    else:
+        setup()
+
+    from django.test.utils import get_runner
+
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
