79f6c5c87773a72e03c17a754af5faf177589ce3
James Ritchie <james.a.ritchie@gmail.com>
2015-02-23 18:59:21 +0000

    Add setup files

---
+++ setup.py
@@ -0,0 +1,57 @@
+"""Setup file for rest_framework_sav."""
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+import rest_framework_sav
+
+
+version = rest_framework_sav.__version__
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='djangorestframework-sav',
+    version=version,
+    description='Session Authentication View for Django REST Framework',
+    url='https://github.com/JamesRitchie/django-rest-framework-sav',
+    author='James Ritchie',
+    author_email='james.a.ritchie@gmail.com',
+    license='BSD',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'djangorestframework>=3.0.5'
+    ],
+    test_suite='runtests.run',
+    tests_require=['Django'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
